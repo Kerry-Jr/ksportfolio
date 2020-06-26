d0bedac624091acad8d182c3bb0f62d4603e2f34
Kerry Smith <kerrysfs@gmail.com>
2020-06-25 23:26:52 -0700

    another iteration for the card layout

--- src/components/Card/cards.component.jsx
+++ src/components/Card/cards.component.jsx
@@ -4,12 +4,12 @@ import './cards.styles.css';
 
 function Card (props) {
     return (
-        <div className="container">
+        <div style={{height: '500px', width: '500px'}}>
             <div className='card-body'>
                 <div className="row">
                     <div className="col-md-3 col-sm-6 item">
                         <div className="card item-card card-block">
-                            <h4 className="card-title text-right"><i className="material-icons">settings</i></h4>
+                            <h4 className="card-title text-right"><i className="material-icons">{props.name}</i></h4>
                             <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg"
                                  alt="screenshot" />
                             <h5 className="item-card-title mt-3 mb-3">Sierra Web Development • Owner</h5>
