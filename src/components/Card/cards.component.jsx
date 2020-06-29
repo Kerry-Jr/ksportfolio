bae80dacc84c9a39427fd5c5997cc1258d6f78e1
Kerry Smith <kerrysfs@gmail.com>
2020-06-29 15:16:11 -0700

    updated card map, added bootstrap buttons with LINK to each card

--- src/components/Card/cards.component.jsx
+++ src/components/Card/cards.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 import './cards.styles.css';
 
@@ -8,17 +8,23 @@ function Card (props) {
     return (
         <div className='col-sm-4 md-6 lg-10 offset-1'>
 
-                    <div className='container'>
+                    <div className='container mt-5'>
                         <div className="card item-card bg-light" key={props.id}>
                             <h4 className="card-title text-center">{props.name}</h4>
                             <img src={props.image} className='card-image-top'
                                  alt="..."/>
-                            <h5 className="item-card-title my-3 text-muted">
+                            <p className="item-card-title my-3 text-muted cText">
                                 {props.technologies}
-                            </h5>
-                            <p className="card-text">
+                            </p>
+                            <p className="card-text cText">
                                 {props.description}
                             </p>
+                            <div className='teleportation'>
+                                <Link target='_blank' to={props.repository} rel='noopener noreferrer' as='button' className='btn-sm btn-danger buttons'>Github Repo</Link>
+                                <Link target='_blank' to={props.liveLink} rel='noopener noreferrer' as='button' className='btn-sm btn-danger buttons'>Live App</Link>
+                            </div>
+
+
                         </div>
                     </div>
         </div>
