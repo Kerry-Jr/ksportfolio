47ef2fb9b2cfbb629c3426ecc55ad00e020e022b
Kerry Smith <kerrysfs@gmail.com>
2020-06-29 14:10:38 -0700

    fade in effect landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import MyNavbar from "./components/Navbar/navbar.component";
 import NewParticles from "./components/Particles/particles.component";
 import Landing from "./pages/Landing/landing.component";
 import About from "./pages/About/about.component";
