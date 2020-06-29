63672a48a012ccf5c005db853a431099ae594402
Kerry Smith <kerrysfs@gmail.com>
2020-06-29 14:12:30 -0700

    updated landing

--- src/pages/Landing/landing.component.jsx
+++ src/pages/Landing/landing.component.jsx
@@ -7,7 +7,7 @@ function Landing() {
      <section className='hero-section'>
        <div className="hero-image">
          <div className="hero-text container">
-           <h1 className='hero-title'>My name is Kerry Smith</h1>
+           <p className='hero-title'>Hello, my name is Kerry Smith</p>
            <p className='hero-decoration'>And I'm a Full-Stack web developer</p>
            <p className='text-muted'>(Click the stars!)</p>
            <Link to='/about' as='button' className='btn-lg btn-danger'>Hire me</Link>
