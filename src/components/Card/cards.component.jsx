6e01f45d1c237cfdc58dd03b5dcb9a739d453518
Kerry Smith <kerrysfs@gmail.com>
2020-07-02 00:18:05 -0700

    added some margin and other css to cards component, also worked out layout for contact form

--- src/components/Card/cards.component.jsx
+++ src/components/Card/cards.component.jsx
@@ -8,15 +8,15 @@ function Card (props) {
     return (
         <div className='col-sm-4 md-6 lg-10 offset-1'>
 
-                    <div className='container mt-5'>
+                    <div className='container-fluid mt-5'>
                         <div className="card item-card bg-light" key={props.id}>
                             <h4 className="card-title text-center">{props.name}</h4>
                             <img src={props.image} className='card-image-top'
                                  alt="..."/>
                             <div className="overlay">
                                 <div className="text"><div className='text text-center'>Enjoy</div><div className='teleportation'>
-                                    <Link target='_blank' to={props.repository} rel='noopener noreferrer' as='button' className='btn-sm btn-danger buttons'>Github Repo</Link>
-                                    <Link target='_blank' to={props.liveLink} rel='noopener noreferrer' as='button' className='btn-sm btn-danger buttons'>Live App</Link>
+                                    <Link target='_blank' to={props.repository} rel='noopener noreferrer' as='button' className='btn-lg btn-danger buttons'>Github Repo</Link>
+                                    <Link target='_blank' to={props.liveLink} rel='noopener noreferrer' as='button' className='btn-lg btn-danger buttons'>Live App</Link>
                                 </div></div>
                             </div>
                             <p className="item-card-title my-3 text-muted cText">
--- src/pages/Contact/contact.component.jsx
+++ src/pages/Contact/contact.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import MyNavbar from '../../components/Navbar/navbar.component'
 import './contact.styles.css';
 
 
 function Contact() {
     return (
         <div className='container-fluid'>
-            CONTACTS PAGE GOES HERE
+          <MyNavbar />
+           <div className='col-md-6 offset-4 d-flex social-icon'>
+
+           </div>
         </div>
     )
 }
