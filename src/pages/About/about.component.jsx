9372fdc1fd28a01fb2419685a835c411ad9c7f76
Kerry Smith <kerrysfs@gmail.com>
2020-07-03 11:02:56 -0700

    edited blurb on about page, changed photo, tested site

--- src/pages/About/about.component.jsx
+++ src/pages/About/about.component.jsx
@@ -14,6 +14,16 @@ function About() {
                         <h1 className='display-3 text-left'>About Me</h1>
                         <img className='myImage' src={kerry_smith} alt='Kerry'/>
                         <p className='about-text'>My name is Kerry Smith. I'm a full stack engineer with interests in front-end Javascript libraries and UI/UX design. When we can visualize the data the design develops naturally, with this concept in mind I find my most creative moments are dictated by the data I'm working with.</p>
+                      <div className='kerry-info'>
+                        <p>
+                          I'm from and currently live and work in the Bay Area. During college I became a bar tender to help pay for school and that taught me great inter-personal skills with many different people. After college, I began working for Vintrex Inc. I did your standard IT work taking care of more than 35 customers in the produce field. My duties include building and setting up new work-stations for individuals. Maintain their in house built by us Server. Managed their networks, SQL server, authentication, etc.</p>
+                          <div className='kerry-info'>
+                            <p className='about-text'>
+                              After that the company's name changed from Vintrex to Parsemony Inc. after the original owner retired. From there I started to get more remote duties and helped maintain customers online store by testing the site, setting up config files, SQL servers, maintaining their shopping cart on the cloud with services such as digital ocean, Amazon AWS, and ftp servers.
+                            </p>
+                          </div>
+
+                      </div>
                     </div>
                     <div className='col kerry'>
                         <h1 className='display-5 text-right'>Languages & Frameworks</h1>
@@ -29,7 +39,7 @@ function About() {
                             <li>Express.js</li>
                             <li>redux</li>
                             <li>Redux-form</li>
-                            <li>Python</li>
+                            <li>Python (work in progress...)</li>
                             <li>mysql & mongo_db</li>
                             <li>Firebase</li>
                             <li>Mongoose.js</li>
