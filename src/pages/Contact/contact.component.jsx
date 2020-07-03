38afd716efaf1bb31494f6b930ece4d3a6602700
Kerry Smith <kerrysfs@gmail.com>
2020-07-03 00:33:54 -0700

    styled contact page, added active links to linked in , github, and email with icons with animation

--- src/pages/Contact/contact.component.jsx
+++ src/pages/Contact/contact.component.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import MyNavbar from '../../components/Navbar/navbar.component'
+import { Link } from 'react-router-dom'
 import './contact.styles.css';
 
 
 function Contact() {
     return (
-        <div className='container-fluid'>
+        <div>
           <MyNavbar />
-           <div className='col-md-6 offset-4 d-flex social-icon'>
+          <h1 className='social-header'>Please contact me @</h1>
+          <div className='container social-platform'>
+            <div className='row'>
+              <div className='col social'>
+               <a href='mailto:kerrysfs@gmail.com' className='text-mail'>
+                 <i className="far fa-envelope social-style"></i>
+               </a>
 
-           </div>
+                <div className='social-text'>
+                  My Email
+                </div>
+              </div>
+              <div className='col social'>
+                <a href="https://github.com/Kerry-jr" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-github social-style"></i>
+                </a>
+                <div className='social-text'>
+                  My Github
+                </div>
+              </div>
+              <div className='col social'>
+                <a href="https://www.linkedin.com/in/kerry-smith-2954221ab/" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-linkedin-in social-style"></i>
+                </a>
+                <div className='social-text'>
+                  My Linked in
+                </div>
+              </div>
+          </div>
+
+          </div>
+
         </div>
     )
 }
