96a9205b0a9340f150505768ce34135bad0d1a7e
Kerry Smith <kerrysfs@gmail.com>
2020-09-21 19:18:41 -0700

    fixed text being blurred out by animated universe on landing

--- src/pages/Landing/landing.component.jsx
+++ src/pages/Landing/landing.component.jsx
@@ -11,7 +11,7 @@ function Landing() {
            <p className='hero-title'><span>Hello, my name is Kerry Smith</span></p>
            <p className='hero-title'></p>
            <p className='hero-decoration'>and I'm a <span className="landing-span">Full-</span>Stack web developer</p>
-           <p>(Click the background!)</p>
+           <p className='landing-instruction'>Click the background!</p>
            {/*<button type="button" className="enter-btn"><a href="/about">Continue</a></button>*/}
            <button type="button" className="enter-btn" onClick={() => window.location.href = '/about'}>
              - ENTER -
